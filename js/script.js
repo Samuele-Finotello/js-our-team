b804f2e45e042c97d17c546d8754e09a0f49555b
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-16 22:11:23 +0200

    [add] feat card js

--- js/script.js
+++ js/script.js
@@ -36,3 +36,24 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+const memberTeam = (member) => {
+  const memberInfo = `<div class="col-4 d-flex">
+          <div class="col-4 p-0">
+            <img class="img-fluid" src="./${member['img']}" alt="">
+          </div>
+          <div class="col-8 bg-dark text-light ps-4 lh-lg py-3">
+            <h4 class="card-title">${member['name']}</h4>
+            <p class="card-text m-0">${member['role']}</p>
+            <p class="card-text text-primary">${member['email']}</p>
+          </div>
+        </div>`
+  return memberInfo;
+}
+
+const ourTeam = document.getElementById('our-team');
+
+for (let i = 0; i < teamMembers.length; i++) {
+  let card = memberTeam(teamMembers[i]);
+  ourTeam.innerHTML += card;
+}
